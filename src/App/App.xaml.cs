0f9dfd442788d51181f76bb164ded4341fa1223d
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-10-20 11:12:21 +0200

    refactor: simplify constructors and remove redundant braces

--- src/App/App.xaml.cs
+++ src/App/App.xaml.cs
@@ -1,15 +1,14 @@
-﻿namespace App
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿namespace App;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
