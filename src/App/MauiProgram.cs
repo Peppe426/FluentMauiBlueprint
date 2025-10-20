f554f38bc5854bc18ed2b1a1a2810d17bc500f18
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-10-20 18:08:15 +0200

    feat(windows): maximize app window on startup

--- src/App/MauiProgram.cs
+++ src/App/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace App;
 
@@ -16,6 +17,30 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        // Maximize the window on startup (Windows only)
+        builder.ConfigureLifecycleEvents(events =>
+        {
+#if WINDOWS
+            events.AddWindows(windows => windows.OnWindowCreated(window =>
+            {
+                try
+                {
+                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                    var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+                    var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                    if (appWindow?.Presenter is Microsoft.UI.Windowing.OverlappedPresenter presenter)
+                    {
+                        presenter.Maximize();
+                    }
+                }
+                catch
+                {
+                    // No-op: if maximizing fails, just continue with normal window
+                }
+            }));
+#endif
+        });
 
     // Register pages/viewmodels for navigation & DI
         builder.Services.AddSingleton<Pages.SettingsPage>();
