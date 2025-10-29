7e75442288f14cafee02c785885c4e8a13cbf09a
Peter Liljeholm <peter.liljeholm@cardiolex.se>
2025-10-29 08:57:09 +0100

    feat(app): simplify resource dictionary and add theme support
    Refactored `App.xaml` to simplify the `ResourceDictionary` structure by moving the `xmlns:conv` namespace declaration to the root `<Application>` tag and reorganizing the `conv:InitialsConverter` resource.

--- src/App/App.xaml.cs
+++ src/App/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace App;
+﻿using App.Services;
+
+namespace App;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
+        ThemeService.LoadSavedTheme();
+        ThemeService.RegisterSystemThemeListener();
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
